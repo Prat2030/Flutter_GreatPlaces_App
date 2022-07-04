3480ec912355696f7cd76863e353fa41b6fa740d
Prat2030 <pratyushjindal2003@gmail.com>
2022-07-04 19:07:38 +0530

    imported location package and created loaction_input widget file

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class LocationInput extends StatefulWidget {
+  // LocationInput({Key? key}) : super(key: key);
+
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
